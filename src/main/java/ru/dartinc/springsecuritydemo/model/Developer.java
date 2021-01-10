147f417d4a915a4a79b3177386af2d764309ffca
DartAxis <Filatyev.a.a@gmail.com>
2021-01-10 14:13:08 +0300

    Initial commit

---
+++ src/main/java/ru/dartinc/springsecuritydemo/model/Developer.java
@@ -0,0 +1,12 @@
+package ru.dartinc.springsecuritydemo.model;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+
+@Data
+@AllArgsConstructor
+public class Developer {
+    private Long id;
+    private String firstName;
+    private String lastNamame;
+}
